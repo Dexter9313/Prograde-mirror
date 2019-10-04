4687f29e451ebc161bca694691da47e9ca96c192
Florian Cabot <florian.cabot@epfl.ch>
2019-10-04 19:22:04 +0200

    Add atmosphere quality parameter

--- prograde/src/ProgradeSettings.cpp
+++ prograde/src/ProgradeSettings.cpp
@@ -38,6 +38,7 @@ ProgradeSettings::ProgradeSettings(QWidget* parent)
 	insertGroup("quality", tr("Quality"), 1);
 	addUIntSetting("texmaxsize", 16, tr("Textures max size (x512)"), 1, 32);
 	addUIntSetting("planetquality", 4, tr("Planet rendering quality"), 1, 6);
+	addUIntSetting("atmoquality", 6, tr("Atmosphere rendering quality"), 1, 8);
 
 	setCurrentIndex(0);
 }
